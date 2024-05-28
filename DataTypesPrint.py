5cda1cb67aa9592c15a606cfbf31bf26775fdb0f
ryuk7728 <ryuk7728@gmail.com>
2024-05-28 19:45:54 +0530

    FirstFile

---
+++ DataTypesPrint.py
@@ -0,0 +1,13 @@
+#Consists of int,float,string and bool
+a=10 #Do not start a variable with a number, you can use underscore/snakecase
+b=4.5
+c="Pranay"
+d=True
+print(a)
+print("Hello",c) #Use comma to print multiple things
+print("Hello World",end="PO") # end denotes what should be the end of the print statement by default its \n
+print("Hello") 
+
+
+
+
