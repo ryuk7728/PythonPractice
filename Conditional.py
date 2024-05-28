c21c211d5891a416e7b871e9937e6a44583e59dc
ryuk7728 <ryuk7728@gmail.com>
2024-05-28 20:12:08 +0530

    Fourth File

---
+++ Conditional.py
@@ -0,0 +1,17 @@
+#Consists of == != <= >= < >
+x='hello'
+y=3
+z=" pranay"
+print(x*3)
+print(x+z)
+print(x==y)
+print(x!=y)
+m = True or False
+n = True and True
+p = not True
+print(m)
+print(n)
+print(p)
+
+
+
