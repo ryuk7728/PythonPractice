487132c06b378a34e09b35abb8eb7b4d6d79473c
ryuk7728 <ryuk7728@gmail.com>
2024-05-29 20:03:23 +0530

    Ninth file

---
+++ Slice.py
@@ -0,0 +1,9 @@
+x=[1,2,3,4,5]
+s="Pranay"
+print(x[0:4:2]) #start:stop:step
+print(x[0:])
+print(x[:4]) #Does not include last index
+print(x[2:3])
+print(x[::-1])
+print(s[::-1]) #Also works on strings
+
